0c7ee315358b54f6a6100940082f964a78a6feff
Veronika Novakova <veronika3novak@gmail.com>
2021-03-23 20:43:19 +0100

    no message

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // tady je místo pro náš program
+
+
+
+function hod() {
+    let diceRoll = Math.floor( Math.random() * 6 ) +1;
+    let text = document.querySelector("#zprava");
+    let kostka = document.querySelector("#kostka");
+
+    if (diceRoll === 6) {
+        text.innerHTML = "Hodil jsi 6. Vyhrál jsi!";
+        console.log("Hodil jsi 6. Vyhrál jsi!");
+        
+    } else {
+        text.innerHTML = "Hodil jsi " + diceRoll + "." + " Zkus to znova.";
+        console.log("Hodil jsi " + diceRoll + "." + " Zkus to znova.");
+    }
+
+    kostka.src = "obrazky/" + diceRoll + ".png";
+}
